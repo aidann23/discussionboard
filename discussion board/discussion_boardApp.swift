4b8cf24c7a1f4b126b5359b54302f7246d8f490a
aidann23 <aidannguyen135@gmail.com>
2024-04-24 14:30:08 -0700

    Initial Commit

---
+++ discussion board/discussion_boardApp.swift
@@ -0,0 +1,32 @@
+//
+//  discussion_boardApp.swift
+//  discussion board
+//
+//  Created by Aidan Nguyen on 4/24/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct discussion_boardApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
